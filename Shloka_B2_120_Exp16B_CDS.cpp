126b484f48e652b3018938c9946a5512b321c35c
Shloka-Patel <137433503+Shloka-Patel@users.noreply.github.com>
2024-09-17 15:05:51 +0530

    Add files via upload

---
+++ Shloka_B2_120_Exp16B_CDS.cpp
@@ -0,0 +1,36 @@
+// NAME - SHLOKA PATEL 
+// PRN - 23070123120 
+// EXPERIMENT - 16(B) 
+
+// EXCEPTION HANDLING 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"Enter age: ";
+    cin>>age;
+    try{
+        if(age<18) {
+            throw age;
+        }
+        else {
+            cout<<"Age: "<<age<<"\n APPROVED"<<endl;
+        }
+    }
+    catch(int a) {
+        cout<<"\n ERROR: Underage! ("<<age<<")"<<endl;
+    }
+}
+
+// Output 
+
+// Enter age: 7 
+//  
+//  ERROR: Underage! (7) 
+
+// Enter age: 20
+// Age: 20
+//  APPROVED 
